72e37c3c3cab747c8b58fa56da82dbb90cf397f7
nikkixploit <34540627+nikkixploit@users.noreply.github.com>
2020-06-23 15:04:33 +0700

    Update run.py

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def main():
     
     if 'Y' or 'y' in w: 
         print ('\033[0;32m sedang membuat...')
-        os.system('cd /sdcard/virus;curl -o virtex.txt https://pastebin.com/raw/x73H685M')
+        os.system('cd /sdcard/virus;curl -o virtex.txt https://pastebin.com/raw/grmsikaB')
         print('\033[37;1m Tersimpan disini sob => sdcard/virus/virtex.txt')
         a = input ('\nSukses, Virus/Virtex Tersimpan Di Folder Virus \n [Ketik Enter Untuk Keluar]')
     else:
